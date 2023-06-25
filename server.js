857202e46692f9ed105dcd7ef71d41dbf0bf8286
Sushmzn9 <sushan.maharjan18@gmail.com>
2023-06-25 10:15:15 +1000

    project initial

---
+++ server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use("/", (req, res) => {
+  res.json({
+    status: "success",
+    message: "server running healthy",
+  });
+});
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error.message)
+    : console.log(`server running at http://localhost:${PORT}`);
+});
